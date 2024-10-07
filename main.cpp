77f25041baeed808cb42742a6101c1f3e2fc436c
ku-rihara <k023g0027@g.neec.ac.jp>
2024-10-07 12:50:49 +0900

    解き直し

--- main.cpp
+++ main.cpp
@@ -8,17 +8,26 @@ int moneyCountRecursion(int workTime) {
 	}
 	return moneyCountRecursion((workTime - 1)) * 2 - 50;
 }
+// 全体のもらえる金額を計算する関数
+int totalMoney(int workTime) {
+	int total = 0;
+	for (int i = 1; i <= workTime; i++) {
+		total += moneyCountRecursion(i);
+	}
+	return total;
+}
 
 int moneyCount(int workTime) {
 	return 1072 * workTime;
 }
 
 int main() {
+	
 
-	for (int workIndex = 1; workIndex <= 9; workIndex++) {
+	for (int workIndex = 1; workIndex <= 8; workIndex++) {
 		printf("一般的な賃金体系:%d\n", moneyCount(workIndex));
-		printf("再帰的な賃金体系:%d\n", moneyCountRecursion(workIndex));
-		if (moneyCount(workIndex) > moneyCountRecursion(workIndex)) {
+		printf("再帰的な賃金体系:%d\n", totalMoney(workIndex));
+		if (moneyCount(workIndex) > totalMoney(workIndex)) {
 			printf("一般的な賃金体系の方が大きい\n");
 		}
 		else {
