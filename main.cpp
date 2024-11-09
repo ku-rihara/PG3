b4cbbbe360c26924cbf15864378c54c5bae08538
ku-rihara <k023g0027@g.neec.ac.jp>
2024-11-09 16:47:37 +0900

    確認課題02_02

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 #include <stdio.h>
-//std
-#include <chrono>
-#include <thread>
-//class
-#include"Enemy.h"
+#include<memory>
+/// class 
+#include"Number.h"
 
 int main() {
-   
-    Enemy* enemy = new Enemy();
- 
-    while (true) {  
-        // フレームごとに敵の状態を更新
-        enemy->Update();
+    std::unique_ptr<Number<int, int>> numI = std::make_unique<Number<int, int>>();
+    std::unique_ptr<Number<float, float>> numF = std::make_unique<Number<float, float>>();
+    std::unique_ptr<Number<double, double>> numD = std::make_unique<Number<double, double>>();
+    std::unique_ptr<Number<int, float>> numIF = std::make_unique<Number<int, float>>();
+    std::unique_ptr<Number<int, double>> numID = std::make_unique<Number<int, double>>();
+    std::unique_ptr<Number<float, double>> numFD = std::make_unique<Number<float, double>>();
 
-        _Thrd_sleep_for(1);//毎フレーム処理    
-    }
+
+    printf("int同士:%d\n", numI->Min(5,3));
+    printf("float同士:%2.1f\n", numF->Min(5.5f, 3.5f));
+    printf("double同士:%2.1lf\n", numD->Min(5.6, 3.9));
+    printf("intとfloat:%d\n", numIF->Min(5, 3.8f));
+    printf("intとdouble:%d\n", numID->Min(5, 4.0));
+    printf("floatとdouble:%2.1f\n", numFD->Min(5.5f, 3.5));
 
     return 0;
 }
