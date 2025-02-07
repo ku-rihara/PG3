677de9fd278f53442fed0cf785371d8462b12e94
ku-rihara <k023g0027@g.neec.ac.jp>
2025-02-07 13:20:08 +0900

    07_01

--- main.cpp
+++ main.cpp
@@ -1,20 +1,33 @@
 #include <stdio.h>
+#include<iostream>
 //std
 #include <chrono>
-#include <thread>
+#include<string>
 //class
 #include"Enemy.h"
 
 int main() {
    
-    Enemy* enemy = new Enemy();
- 
-    while (true) {  
-        // フレームごとに敵の状態を更新
-        enemy->Update();
+    std::string a(100000,'a');
+   
+    // コピー
+    auto startCopy = std::chrono::high_resolution_clock::now();
+    std::string copy = a;
+    auto endCopy = std::chrono::high_resolution_clock::now();
 
-        _Thrd_sleep_for(1);//毎フレーム処理    
-    }
+    // 移動
+    auto startMove = std::chrono::high_resolution_clock::now();
+    std::string move = std::move(a);
+    auto endMove = std::chrono::high_resolution_clock::now();
+  
+
+    std::cout << "コピーの時間: "
+        << std::chrono::duration_cast<std::chrono::microseconds>(endCopy - startCopy).count()
+        << " μs\n";
+
+    std::cout << "移動の時間: "
+        << std::chrono::duration_cast<std::chrono::microseconds>(endMove - startMove).count()
+        << " μs\n";
 
     return 0;
 }
