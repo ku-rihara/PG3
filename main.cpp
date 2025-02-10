4c4a4cba5fd422888fbf88179e0adf3a8191bc70
ku-rihara <k023g0027@g.neec.ac.jp>
2025-02-10 10:17:41 +0900

    ホワイトスミス修正

--- main.cpp
+++ main.cpp
@@ -14,53 +14,52 @@ typedef int (*DiceDecision)(int*);
 
 // サイコロを振る関数
 int rollDice(int* result) {
-    *result = dist(random);
-    return *result;
+	*result = dist(random);
+	return *result;
 }
 
 void showDot() {
-    printf(".");
+	printf(".");
 }
 
 void SetTimeout(void (*func)()) {
-    printf(".");
-    std::this_thread::sleep_for(std::chrono::seconds(1)); // 3秒待機
+	printf(".");
+	std::this_thread::sleep_for(std::chrono::seconds(1)); // 3秒待機
 
 }
 
 //サイコロゲーム関数
 void PlayDiceGame(std::function<int(int*)>diceDecisionFunc) {
-    printf("奇数か偶数か当ててください(奇数:1、偶数:2)\n");
-    int answer;
-    scanf_s("%d", &answer); // 奇数偶数を当ててもらう
+	printf("奇数か偶数か当ててください(奇数:1、偶数:2)\n");
+	int answer;
+	scanf_s("%d", &answer); // 奇数偶数を当ててもらう
 
-    int result;
+	int result;
 
-    printf("正解は");
-    for (int i = 0; i < 3; ++i) {
-        SetTimeout(showDot);
-    }
-    printf("\n");
+	printf("正解は");
+	for (int i = 0; i < 3; ++i) {
+		SetTimeout(showDot);
+	}
+	printf("\n");
 
-    diceDecisionFunc(&result); //コールバック関数
-    printf("出目は %d です。\n", result);
+	diceDecisionFunc(&result); //コールバック関数
+	printf("出目は %d です。\n", result);
 
-    // 判定
-    bool isOdd = result % 2 != 0; // 奇数かどうかの判定
-    if ((answer == 1 && isOdd) || (answer == 2 && !isOdd)) {
-        printf("正解です\n");
-    }
-    else {
-        printf("不正解です\n");
-    }
+	// 判定
+	bool isOdd = result % 2 != 0; // 奇数かどうかの判定
+	if ((answer == 1 && isOdd) || (answer == 2 && !isOdd)) {
+		printf("正解です\n");
+	} else {
+		printf("不正解です\n");
+	}
 }
 
 int main() {
-    std::function<int(int*)>function = [](int* i) {return rollDice(i);};
+	std::function<int(int*)>function = [](int* i) {return rollDice(i); };
 
-    // ゲームを開始
-    PlayDiceGame(function);
+	// ゲームを開始
+	PlayDiceGame(function);
 
 
-    return 0;
+	return 0;
 }
